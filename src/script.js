07e56ff29823ef1e7209f785355cbd997196bcdc
jsanders25 <114096326+jsanders25@users.noreply.github.com>
2023-11-29 17:51:56 +0000

    search no work

--- src/script.js
+++ src/script.js
@@ -1,34 +1,56 @@
+// ENDPOINT
+const API_ENDPOINT = 'https://api.giphy.com/v1/gifs/search?'
 // API Key
 const API_KEY = 'UMdM5eoKJZb8EftwDWInYS1AYsWtOQrd';
-// Search term
-// const searchTerm = 'coffee';
+// limit
+const LIMIT = 25; 
+// rating
+const RATING = 'r';
+// search
+let SEARCH = 'coffee';
 
-// Go fetch Giphy API data
-    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=25&rating=r`)
-      .then( response => response.json() )
-      .then( gifs => {
-      
-        // Check Check         
-        console.log(gifs.data);
-        console.log(gifs.data[2].images.downsized);
-      
-      // Get container for data
-      const container = document.querySelector('.container');         
-      
-       // Loop through the array of data
-      gifs.data.map( gif => {
-        
-          // template 
-          const template  = `
-          <div class="carousel-item w-full">
-            <img src=" ${gif.images.downsized.url} " />
-             <p> ${gif.title} </p>
-		    </div>
-          `;
-        
-          // append
-          container.insertAdjacentHTML("afterbegin", template);
-        
-      });
-      
+// Get `input` element
+const inputField = document.querySelector('input[type="text"]');
+
+
+inputField.addEventListener("keyup", event => {
+  
+  
+  // the search string    
+  console.log(inputField.value);
+  
+    // Go fetch Giphy API data
+    fetch(`
+    ${API_ENDPOINT}api_key=${API_KEY}&q=${inputField.value}&limit=${LIMIT}&rating=${RATING}
+    `)
+    .then( response => response.json() )
+    .then( gifs => {
+
+    // Check Check         
+    console.log(gifs.data[0].images.downsized);
+
+    // Get container for data
+    const container = document.querySelector('.container');         
+
+    // Loop through the array of data
+    gifs.data.map( gif => {
+
+    // template 
+    const template  = `
+    <div class="carousel-item w-full">
+      <img class="border-8 border-white" src=" ${gif.images.downsized.url} " />
+      <h2 class="text-5xl text-center text-white"> ${gif.title} </h2>
+    </div>
+    `;
+
+    // append
+    container.insertAdjacentHTML("afterbegin", template);
+
     });
+
+
+    });
+
+
+
+});
